f6f75365182189f6d4db09ca20d03c1f7bd1fb89
g-ojo <ojogideon@yahoo.com>
2018-10-26 22:26:49 -0400

    Some refractoring to make functions, methods, and classes more clear

---
+++ CarRentalCompany/RentalSubmissionForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalCompany
+{
+    public partial class RentalSubmissionForm : Form
+    {
+        string firstname, lastname, emailaddr, phonenum, age, vehicalType;
+        DateTime pickuptime, dropofftime;
+        bool gasEmis, gps, addDriver, satRadio, roadAssist; 
+        public RentalSubmissionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            hideAllForm();
+        }
+
+        public void hideAllForm()
+        {
+            firstNameTextBox.Hide();
+            lastNameTextBox.Hide();
+            ageComboBox.Hide();
+            emailTextBox.Hide();
+            phoneNumberTextBox.Hide();
+            pickupDatePicker.Hide();
+            dropoffDatePicker.Hide();
+            vehicleComboBox.Hide();
+            addOnsCheckedListBox.Hide();
+            submitButton.Hide();
+        }
+    }
+}
