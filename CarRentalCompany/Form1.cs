2cc016fe00765b3f4c7d446efc491f615c254165
Katie Elizabeth Vick <10608995@live.mercer.edu>
2018-10-26 21:40:02 -0400

    added hide function so that when the submit button is clicked all inputs are hidden and an output is displayed. Variables created.

--- CarRentalCompany/Form1.cs
+++ CarRentalCompany/Form1.cs
@@ -12,9 +12,13 @@ namespace CarRentalCompany
 {
     public partial class Form1 : Form
     {
+        string firstname, lastname, emailaddr, phonenum, age, vehicalType;
+        DateTime pickuptime, dropofftime;
+        bool gasEmis, gps, addDriver, satRadio, roadAssist; 
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
@@ -35,6 +39,31 @@ namespace CarRentalCompany
         private void vehicleComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            hideAllForm();
+            
+        }
+
+        public void hideAllForm()
+        {
+            firstName.Hide();
+            lastName.Hide();
+            ageComboBox.Hide();
+            email.Hide();
+            phoneNumber.Hide();
+            pickup.Hide();
+            dropoff.Hide();
+            vehicleComboBox.Hide();
+            addOnsCheckedBox.Hide();
+            submit.Hide();
         }
     }
 }
